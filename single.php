3f18a5e746a0cdca3d274ff18eb1c795fbe30b04
Adam Fortuna <adam@fortuna.name>
2016-07-30 14:13:28 -0400

    Added related posts

--- single.php
+++ single.php
@@ -29,6 +29,28 @@ while ( have_posts() ) : the_post();
 
 <?php } ?>
 
+
+
+
+<div class='container'>
+	<section class="related-posts <?php minafi_columns() ?>">
+		<h3>Related Posts</h3>
+
+		<section class='articles--list related-posts--articles'>
+		<?php
+			$related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 5, 'post__not_in' => array($post->ID) ) );
+			if( $related ) foreach( $related as $post ) {
+				setup_postdata($post);
+				?>
+					<?php get_template_part( 'template-parts/content-title', get_post_format() ); ?>
+			<?php }
+			wp_reset_postdata();
+		?>
+		</section>
+  </section>
+</div>
+
+
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
