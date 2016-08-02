1721ef99ef5ff6fa188f66a6036eddd0f9c2a54e
Adam Fortuna <adam@fortuna.name>
2016-08-01 23:14:46 -0600

    Added ability for posts to not have a thumbnail

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -6,10 +6,16 @@
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
+
+ $thumbnail_class = '';
+
+ if(has_post_thumbnail()) {
+   $thumbnail_class = 'article--has-thumbnail';
+ }
 ?>
 
 <div class='container article-single--wrapper'>
-	<article id="post-<?php the_ID(); ?>" <?php post_class('article--singular'); ?> >
+	<article id="post-<?php the_ID(); ?>" <?php post_class(['article--singular', $thumbnail_class]); ?> >
 
 		<header class="row article--header">
 			<?php minafi_post_thumbnail() ?>
--- template-parts/content-title.php
+++ template-parts/content-title.php
@@ -6,8 +6,15 @@
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
+
+$thumbnail_class = '';
+
+if(has_post_thumbnail()) {
+  $thumbnail_class = 'article--has-thumbnail';
+}
+
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class($thumbnail_class); ?>>
   <a href='<?php echo esc_url(get_permalink()); ?>'>
     <?php minafi_post_thumbnail('thumbnail', true); ?>
     <?php minafi_title() ?>
