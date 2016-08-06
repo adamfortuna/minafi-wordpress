1db31caa7b10ca965adb93e1c5f6f44aa4790c88
Adam Fortuna <adam@fortuna.name>
2016-08-06 01:08:51 -0400

    Started reorganizing functions

--- functions.php
+++ functions.php
@@ -261,12 +261,12 @@ function twentysixteen_hex2rgb( $color ) {
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require get_template_directory() . '/src/php/template-tags.php';
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+require get_template_directory() . '/src/php/customizer.php';
 
 /**
  * Add custom image sizes attribute to enhance responsive image functionality
@@ -337,30 +337,6 @@ function new_excerpt_more( $more ) {
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
 
-
-
-// function disable_wp_emojicons() {
-//   // all actions related to emojis
-//   remove_action( 'admin_print_styles', 'print_emoji_styles' );
-//   remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-//   remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-//   remove_action( 'wp_print_styles', 'print_emoji_styles' );
-//   remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-//   remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-//   remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-//
-//   // filter to remove TinyMCE emojis
-//   add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
-// }
-// add_action( 'init', 'disable_wp_emojicons' );
-
-
-
-function twentyten_remove_recent_comments_style() {
-    global $wp_widget_factory;
-    remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
-}
-add_action( 'widgets_init', 'twentyten_remove_recent_comments_style' );
 
 
 /**
--- src/php/template-tags.php
+++ src/php/template-tags.php
@@ -57,6 +57,7 @@ endif;
 
 
 
+if ( ! function_exists( 'bootstrap3_comment_form_fields' ) ) :
 add_filter( 'comment_form_default_fields', 'bootstrap3_comment_form_fields' );
 function bootstrap3_comment_form_fields( $fields ) {
     $commenter = wp_get_current_commenter();
@@ -76,8 +77,9 @@ function bootstrap3_comment_form_fields( $fields ) {
 
     return $fields;
 }
+endif;
 
-
+if ( ! function_exists( 'bootstrap3_comment_form' ) ) :
 add_filter( 'comment_form_defaults', 'bootstrap3_comment_form' );
 function bootstrap3_comment_form( $args ) {
     $args['comment_field'] = '<div class="form-group comment-form-comment">
@@ -88,11 +90,220 @@ function bootstrap3_comment_form( $args ) {
 
     return $args;
 }
+endif;
 
-
+if ( ! function_exists( 'bootstrap3_comment_button' ) ) :
 add_action('comment_form', 'bootstrap3_comment_button' );
 function bootstrap3_comment_button() {
     echo '<button class="btn btn-outline-primary" type="submit">' . __( 'Submit Comment' ) . '</button>';
 }
+endif;
+
+
+
+
+
+
+
+
+
+
+if ( ! function_exists( 'twentysixteen_entry_meta' ) ) :
+/**
+ * Prints HTML with meta information for the categories, tags.
+ *
+ * Create your own twentysixteen_entry_meta() function to override in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ */
+function twentysixteen_entry_meta() {
+	if ( 'post' === get_post_type() ) {
+		$author_avatar_size = apply_filters( 'twentysixteen_author_avatar_size', 49 );
+		printf( '<span class="byline"><span class="author vcard">%1$s<span class="screen-reader-text">%2$s </span> <a class="url fn n" href="%3$s">%4$s</a></span></span>',
+			get_avatar( get_the_author_meta( 'user_email' ), $author_avatar_size ),
+			_x( 'Author', 'Used before post author name.', 'twentysixteen' ),
+			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+			get_the_author()
+		);
+	}
+
+	if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
+		twentysixteen_entry_date();
+	}
+
+	$format = get_post_format();
+	if ( current_theme_supports( 'post-formats', $format ) ) {
+		printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
+			sprintf( '<span class="screen-reader-text">%s </span>', _x( 'Format', 'Used before post format.', 'twentysixteen' ) ),
+			esc_url( get_post_format_link( $format ) ),
+			get_post_format_string( $format )
+		);
+	}
+
+	if ( 'post' === get_post_type() ) {
+		twentysixteen_entry_taxonomies();
+	}
+
+	if ( ! is_singular() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'twentysixteen' ), get_the_title() ) );
+		echo '</span>';
+	}
+}
+endif;
+
+if ( ! function_exists( 'twentysixteen_entry_date' ) ) :
+/**
+ * Prints HTML with date information for current post.
+ *
+ * Create your own twentysixteen_entry_date() function to override in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ */
+function twentysixteen_entry_date() {
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		get_the_date(),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		get_the_modified_date()
+	);
+
+	printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
+		_x( 'Posted on', 'Used before publish date.', 'twentysixteen' ),
+		esc_url( get_permalink() ),
+		$time_string
+	);
+}
+endif;
+
+if ( ! function_exists( 'minafi_title' ) ) :
+function minafi_title() { ?><h2><?php the_title(); ?></h2><?php }
+endif;
+
+
+if ( ! function_exists( 'minafi_post_thumbnail' ) ) :
+/**
+ * Displays an optional post thumbnail.
+ *
+ * Wraps the post thumbnail in an anchor element on index views, or a div
+ * element when on single views.
+ *
+ * Create your own twentysixteen_post_thumbnail() function to override in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ */
+function minafi_post_thumbnail($size = 'large') {
+	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+		return;
+	}
+
+	if ( is_singular() && $size != 'thumbnail') :
+	?>
+
+	<div class="post-thumbnail">
+		<?php the_post_thumbnail($size); ?>
+	</div><!-- .post-thumbnail -->
+
+	<?php else : ?>
+		<div class="post-thumbnail thumbnail--small">
+			<?php the_post_thumbnail('thumbnail'); ?>
+		</div>
+	<?php endif; // End is_singular()
+}
+endif;
+
+if ( ! function_exists( 'minafi_post_thumbnail_background' ) ) :
+function minafi_post_thumbnail_background($size = 'large') {
+	if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+		return;
+	}
+
+	return the_post_thumbnail_url($size);
+}
+endif;
+
+if ( ! function_exists( 'minafi_excerpt' ) ) :
+	/**
+	 * Displays the optional excerpt.
+	 *
+	 * Wraps the excerpt in a div element.
+	 *
+	 * Create your own twentysixteen_excerpt() function to override in a child theme.
+	 *
+	 * @since Twenty Sixteen 1.0
+	 *
+	 * @param string $class Optional. Class string of the div element. Defaults to 'entry-summary'.
+	 */
+	function minafi_excerpt( $class = 'lead' ) {
+		$class = esc_attr( $class );
+
+		if ( has_excerpt()) : ?>
+			<div class="<?php echo $class; ?>">
+				<?php the_excerpt(); ?>
+			</div>
+		<?php endif;
+	}
+endif;
+
+if ( ! function_exists( 'twentysixteen_excerpt_more' ) && ! is_admin() ) :
+/**
+ * Replaces "[...]" (appended to automatically generated excerpts) with ... and
+ * a 'Continue reading' link.
+ *
+ * Create your own twentysixteen_excerpt_more() function to override in a child theme.
+ *
+ * @since Twenty Sixteen 1.0
+ *
+ * @return string 'Continue reading' link prepended with an ellipsis.
+ */
+function twentysixteen_excerpt_more() {
+	$link = sprintf( '<a href="%1$s" class="more-link">%2$s</a>',
+		esc_url( get_permalink( get_the_ID() ) ),
+		/* translators: %s: Name of current post */
+		sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ), get_the_title( get_the_ID() ) )
+	);
+	return ' &hellip; ' . $link;
+}
+add_filter( 'excerpt_more', 'twentysixteen_excerpt_more' );
+endif;
+
+
+
+/**
+ * Flushes out the transients used in twentysixteen_categorized_blog().
+ *
+ * @since Twenty Sixteen 1.0
+ */
+// function twentysixteen_category_transient_flusher() {
+// 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+// 		return;
+// 	}
+// 	// Like, beat it. Dig?
+// 	delete_transient( 'twentysixteen_categories' );
+// }
+// add_action( 'edit_category', 'twentysixteen_category_transient_flusher' );
+// add_action( 'save_post',     'twentysixteen_category_transient_flusher' );
+
+if ( ! function_exists( 'twentysixteen_the_custom_logo' ) ) :
+/**
+ * Displays the optional custom logo.
+ *
+ * Does nothing if the custom logo is not available.
+ *
+ * @since Twenty Sixteen 1.2
+ */
+function twentysixteen_the_custom_logo() {
+	if ( function_exists( 'the_custom_logo' ) ) {
+		the_custom_logo();
+	}
+}
+endif;
+
 
 ?>
