4cb50135d67398c2ae7b898f586f1dd206567e40
Adam Fortuna <adam@fortuna.name>
2017-07-04 12:40:19 -0400

    Added header area to homepage

--- index.php
+++ index.php
@@ -14,36 +14,10 @@
  * @since Twenty Sixteen 1.0
  */
 
-get_header(); ?>
+get_header();
 
-<div class="container first-content">
-	<div class='row'>
-    <section class="welcome jumbotron <?php minafi_columns(); ?>">
-			<p>Hey hey! I'm Adam Fortuna, and I explore the intersection of
-      <a href='/articles/category/minimalism' class='minimalism--text'>minimalism</a>,
-      <a href='/articles/category/financial-independence' class='fi--text'>financial independence</a> and
-      <a href='/articles/category/mindfulness' class='mindfulness--text'>mindfulness</a>.</p>
-
-			<p>Read more about <a href="/about">what these topics have in common</a>, or jump into an area that looks interesting to you!</a>
-
-			<form class="form-inline" action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=minafi', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
-				<div class="form-group">
-	    		<input type="text" name="email" class="form-control form-control-sm email-address" id="emailAddress" placeholder="What's your email address?" width='100'>
-				</div>
-
-				<input type="hidden" value="minafi" name="uri"/>
-				<input type="hidden" name="loc" value="en_US"/>
-
-				<button type="submit" class="btn btn-outline-primary btn-sm">
-					<i class="fa fa-envelope" aria-hidden="true"></i>
-				  Subscribe to Minafi
-				</button>
-
-				<small class="form-text text-muted">We'll keep your email secret and only email you when new posts come out.</small>
-			</form>
-    </section>
-	</div>
-</div>
+include('partials/hero.php');
+?>
 
 <div class="container">
 	<section class="<?php minafi_columns(); ?> latest-article">
---
+++ partials/hero.php
@@ -0,0 +1,11 @@
+  <div class="home--hero">
+    <div class="home--hero-wrap">
+    <div class="container home--text">
+      <h1><?php echo get_bloginfo("name"); ?></h1>
+      <p>
+        <?php echo get_bloginfo("description"); ?>
+        <a href="/about">Read more</a>
+      </p>
+    </div>
+  </div>
+</div>
