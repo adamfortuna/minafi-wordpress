23a048ca251834f63af46a804d8351a2746641e3
Adam Fortuna <adam@fortuna.name>
2017-07-05 19:52:20 -0400

    Fixing up the about page

--- page.php
+++ page.php
@@ -13,26 +13,22 @@
 
 get_header(); ?>
 
-<div class='container first-content container-slim'>
-	<div class='row'>
-		<section class="<?php minafi_columns(); ?>">
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
+<section class="aesop-entry-content first-content">
+	<?php
+	// Start the loop.
+	while ( have_posts() ) : the_post();
 
-				// Include the page content template.
-				get_template_part( 'template-parts/content', 'page' );
+		// Include the page content template.
+		get_template_part( 'template-parts/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) {
+			comments_template();
+		}
 
-				// End of the loop.
-			endwhile;
-			?>
-		</section>
-	</div>
-</div>
+		// End of the loop.
+	endwhile;
+	?>
+</section>
 
 <?php get_footer(); ?>
