1d214b643cdac0dcc619fa5432d86ea281f856a0
Adam Fortuna <adam@fortuna.name>
2017-07-11 23:46:24 -0400

    Added social integration for fb and twitter

---
+++ partials/social.php
@@ -0,0 +1,20 @@
+<div class="sidebar--group">
+  <h3>Facebook</h3>
+
+  <!-- Facebook -->
+  <div class="fb-page" data-href="https://www.facebook.com/minafiblog/" data-tabs="timeline" data-width="315" data-height="210" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/minafiblog/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/minafiblog/">Minafi</a></blockquote></div>
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=200902338079";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+</div>
+
+<div class="sidebar--group">
+  <h3>Twitter</h3>
+  <!-- Twitter -->
+  <a class="twitter-timeline" data-width="315" data-height="220" href="https://twitter.com/minafiblog">Tweets by minafiblog</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
