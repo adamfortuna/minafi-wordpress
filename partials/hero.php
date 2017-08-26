6e659bc3e83c92c2fffc63fdf5bb80dc718ba91d
Adam Fortuna <adam@fortuna.name>
2017-08-26 16:10:04 -0600

    Moving title higher and fixing mobile scrolling

--- partials/hero.php
+++ partials/hero.php
@@ -1,12 +1,14 @@
 <div class="home--hero mt-3">
   <div class="home--hero-content container wide-container home--text">
     <div class='col-md-8 col-12 hero--text px-3'>
-      <img src="<?php echo get_template_directory_uri() ?>/assets/images/mfi.png" height="100" width="100" class="float-left mr-3 pr-3" />
-      <h1><?php echo get_bloginfo("name"); ?></h1>
-      <p>
-        <?php echo get_bloginfo("description"); ?>
-        <a href="/about">Read more</a>
-      </p>
+      <div class="hero--text-content">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/mfi.png" height="100" width="100" class="float-left mr-3 pr-3" />
+        <h1><?php echo get_bloginfo("name"); ?></h1>
+        <p>
+          <?php echo get_bloginfo("description"); ?>
+          <a href="/about">Read more</a>
+        </p>
+      </div>
     </div>
   </div>
 </div>
