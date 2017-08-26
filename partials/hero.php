3f85d38743f56a740c003b53d30c42408b32d649
Adam Fortuna <adam@fortuna.name>
2017-08-26 15:48:11 -0600

    Changed up sidebar and hero area

--- partials/hero.php
+++ partials/hero.php
@@ -1,16 +1,12 @@
-  <div class="home--hero">
-    <div class="home--hero-wrap">
-    <div class="container wide-container home--text">
-      <div class='col-12 hero--text'>
-        <p>
-          <img src="<?php echo get_template_directory_uri() ?>/assets/images/mfi.png" height="100" width="100" />
-        </p>
-        <h1><?php echo get_bloginfo("name"); ?></h1>
-        <p>
-          <?php echo get_bloginfo("description"); ?>
-          <a href="/about">Read more</a>
-        </p>
-      </div>
+<div class="home--hero mt-3">
+  <div class="home--hero-content container wide-container home--text">
+    <div class='col-md-8 col-12 hero--text px-3'>
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/mfi.png" height="100" width="100" class="float-left mr-3 pr-3" />
+      <h1><?php echo get_bloginfo("name"); ?></h1>
+      <p>
+        <?php echo get_bloginfo("description"); ?>
+        <a href="/about">Read more</a>
+      </p>
     </div>
   </div>
 </div>
