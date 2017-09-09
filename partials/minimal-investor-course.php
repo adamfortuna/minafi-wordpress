4e9186ac7d094e11e96ea8198baf49b524ec7aea
Adam Fortuna <adam@fortuna.name>
2017-09-09 11:53:01 -0400

    Switched form to Drip

--- partials/minimal-investor-course.php
+++ partials/minimal-investor-course.php
@@ -1,25 +1,14 @@
-<form action="//minafi.us16.list-manage.com/subscribe/post?u=d2476f8b6bf5aa45f4014a563&amp;id=704b6324f7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate target="_blank">
-  <h3>The Minimal Investor Course</h3>
-	<p><small>Sign up to become a <a href="/minimal-investor-course" target="_blank">Minimal Investor</a> with this free 10-week email course.</small></p>
+<form action="https://www.getdrip.com/forms/590874722/submissions" method="post" data-drip-embedded-form="590874722">
+  <h3 data-drip-attribute="headline">The Minimal Investor Course</h3>
+  <p><small>Sign up to become a <a href="/minimal-investor-course" target="_blank">Minimal Investor</a> with this free 10-week email course.</small></p>
 
-	<div class="form-group">
-		<input type="email" placeholder="Email" value="" name="EMAIL" class="email--signup required email form-control form-control-sm">
-	</div>
-
-  <div class="email--toggle" style="display: none;">
-    <div class="form-group">
-    	<input type="text" placeholder="Your first name" value="" name="FNAME" class="email form-control form-control-sm">
-    </div>
+  <div class="form-group">
+    <input type="email" placeholder="Email" value="" name="fields[email]" class="email--signup email form-control form-control-sm">
   </div>
-
-	<div id="mce-responses" class="group">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>
-
-  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d2476f8b6bf5aa45f4014a563_704b6324f7" tabindex="-1" value=""></div>
-
+  <div class="email--toggle mb-2" style="display: none;">
+      <input type="text" id="drip-name" name="fields[name]" value="" placeholder="Your first name" class="email form-control form-control-sm" />
+  </div>
   <div class="group">
-		<button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-sm btn-success">Subscribe</button>
-	</div>
+    <input type="submit" name="submit" value="Sign Up" data-drip-attribute="sign-up-button" class="btn btn-sm btn-success" />
+  </div>
 </form>
