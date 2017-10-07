b2fde79349780a26d1c27a65a5c47ca2316e3c6f
Adam Fortuna <adam@fortuna.name>
2017-10-07 12:55:51 -0400

    Add hover styles to sidebar

--- partials/fi.php
+++ partials/fi.php
@@ -1,14 +1,13 @@
 <div class="mb-3 pb-1">
-  <p class="text-secondary"><small class="float-right">Featured Content <i class="fa fa-level-down" aria-hidden="true"></i></small></p>
-  <p>
-    <a href="https://minafi.com/interactive-guide-early-retirement-financial-independence/">
-      <img src="https://minafi.com/wp-content/uploads/2017/09/Header-Twitter-sidebar-400x189.jpg" alt="An Interactive Guide to Early Retirement and Financial Independence" />
-    </a>
-  </p>
+  <p class="text-secondary text-right mb-0"><small>Featured Content <i class="fa fa-level-down" aria-hidden="true"></i></small></p>
 
-  <p class="mt-3">
-    <a href="https://minafi.com/minimal-investor-course/">
-      <img src="https://minafi.com/wp-content/uploads/2017/09/minafi.com-2-400x200.png" alt="Minimal Investor Course" />
-    </a>
-  </p>
+  <a class="cover--image" href="https://minafi.com/interactive-guide-early-retirement-financial-independence/">
+    <div class="cover--image-overlay"></div>
+    <img src="https://minafi.com/wp-content/uploads/2017/09/Header-Twitter-sidebar-400x189.jpg" alt="An Interactive Guide to Early Retirement and Financial Independence" />
+  </a>
+
+  <a class="mt-3 cover--image" href="https://minafi.com/minimal-investor-course/">
+    <div class="cover--image-overlay"></div>
+    <img src="https://minafi.com/wp-content/uploads/2017/09/minafi.com-2-400x200.png" alt="Minimal Investor Course" />
+  </a>
 </div>
