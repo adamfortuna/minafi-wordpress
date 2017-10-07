ade01051758a8bb8deda25b62a87854b3d3bdcdb
Adam Fortuna <adam@fortuna.name>
2017-10-07 00:30:45 -0400

    Added images

--- partials/email-sidebar.php
+++ partials/email-sidebar.php
@@ -1,7 +1,7 @@
 <div id="mc_embed_signup" class="sidebar--group sidebar--email">
   <form action="https://www.getdrip.com/forms/719052090/submissions" method="post" data-drip-embedded-form="620724064">
-    <h3 data-drip-attribute="headlines">The Minafi Magazine</h3>
-    <p><small data-drip-attribute="descriptions">A dose of investment advice, minimalism and mindfulness <b>delivered just for you</b>.</small></p>
+    <h3 data-drip-attribute="headline">The Minafi Magazine</h3>
+    <p><small data-drip-attribute="description">A dose of investment advice, minimalism and mindfulness <b>delivered just for you</b>.</small></p>
 
     <div class="form-group">
       <input type="email" placeholder="What's Your Email?" value="" name="fields[email]" class="email--signup email form-control form-control-sm">
@@ -15,7 +15,7 @@
       </small>
     </p>
     <div class="group">
-      <input type="submit" name="submit" value="Subscribe" data-drip-attribute="sign-up-buttons" class="btn btn-sm btn-success btn-block email-submit" />
+      <input type="submit" name="submit" value="Subscribe" data-drip-attribute="sign-up-button" class="btn btn-sm btn-success btn-block email-submit" />
     </div>
   </form>
 </div>
--- partials/fi.php
+++ partials/fi.php
@@ -2,13 +2,13 @@
   <p class="text-secondary"><small class="float-right">Featured Content <i class="fa fa-level-down" aria-hidden="true"></i></small></p>
   <p>
     <a href="https://minafi.com/interactive-guide-early-retirement-financial-independence/">
-      <img src="https://minafi.com/wp-content/uploads/2017/09/Header-Twitter-sidebar.jpg" alt="An Interactive Guide to Early Retirement and Financial Independence" />
+      <img src="https://minafi.com/wp-content/uploads/2017/09/Header-Twitter-sidebar-400x189.jpg" alt="An Interactive Guide to Early Retirement and Financial Independence" />
     </a>
   </p>
 
   <p class="mt-3">
     <a href="https://minafi.com/minimal-investor-course/">
-      <img src="https://minafi.com/wp-content/uploads/2017/09/minafi.com-2.png" alt="Minimal Investor Course" />
+      <img src="https://minafi.com/wp-content/uploads/2017/09/minafi.com-2-400x200.png" alt="Minimal Investor Course" />
     </a>
   </p>
 </div>
