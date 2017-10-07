16e18ae52ee26c4b23529b15194e59b0c76d9e5c
Adam Fortuna <adam@fortuna.name>
2017-10-07 00:01:47 -0400

    Updating email signup form

--- partials/email-sidebar.php
+++ partials/email-sidebar.php
@@ -1,3 +1,21 @@
 <div id="mc_embed_signup" class="sidebar--group sidebar--email">
-  <?php include(get_template_directory().'/partials/email.php'); ?>
+  <form action="https://www.getdrip.com/forms/719052090/submissions" method="post" data-drip-embedded-form="620724064">
+    <h3 data-drip-attribute="headlines">The Minafi Magazine</h3>
+    <p><small data-drip-attribute="descriptions">A dose of investment advice, minimalism and mindfulness <b>delivered just for you</b>.</small></p>
+
+    <div class="form-group">
+      <input type="email" placeholder="What's Your Email?" value="" name="fields[email]" class="email--signup email form-control form-control-sm">
+    </div>
+    <p>
+      <small class="pl-2">
+        <input type="radio" name="fields[subscription]" value="all" id="email--subscription-all" checked="checked" /> <label for="email--subscription-all"><small>Weekly Magazine</small></label>
+      </small>
+      <small class="pl-3">
+        <input type="radio" name="fields[subscription]" value="monthly" id="email--subscription-monthly" /> <label for="email--subscription-monthly"><small>Monthly Magazine</small></label>
+      </small>
+    </p>
+    <div class="group">
+      <input type="submit" name="submit" value="Subscribe" data-drip-attribute="sign-up-buttons" class="btn btn-sm btn-success btn-block email-submit" />
+    </div>
+  </form>
 </div>
