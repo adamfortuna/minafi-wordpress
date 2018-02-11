68aa15c9e80dbb1443be4251e836cd0de0c1fdc4
Adam Fortuna <adam@fortuna.name>
2018-02-11 00:11:43 -0700

    Typography changes for articles

--- assets/dev/app.bundle.js
+++ assets/dev/app.bundle.js
@@ -6411,12 +6411,40 @@ window.addComment = {
     }
   }
 
+  function subscribeToDrip(e)  {
+    var form = $('.comment-form'),
+        subscriptionFields = form.find("input[name='subscription[]']:checked");
+    if(subscriptionFields.length > 0) {
+      var email = form.find('#email').val(),
+          name = form.find('#author').val(),
+          url = form.find('#url').val(),
+          subscriptions = $.map(subscriptionFields, function(i) {
+            return $(i).val();
+          }).join(",");
+      $.ajax({
+        type: "POST",
+        url: 'https://www.getdrip.com/forms/194467518/submissions',
+        data: {
+          fields: {
+            email: email,
+            name: name,
+            url: url,
+            subscription: subscriptions
+          }
+        }
+      });
+    }
+  }
+
 
   $(function(){
     $('.search--link').on('click', toggleSearch);
 
     $('.email--signup').on('focus', function() {
       $('.email--toggle').slideDown();
-    })
+    });
+
+
+    $('.comment-form').on('submit', subscribeToDrip);
   });
 })();
