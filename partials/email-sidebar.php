2ad5ecd6ca0051983de08fe45d8629aa8466a34f
Adam Fortuna <adam@fortuna.name>
2018-06-03 14:54:39 -0600

    Added name to homepage email form

--- partials/email-sidebar.php
+++ partials/email-sidebar.php
@@ -4,17 +4,22 @@
     <p><small>A dose of financial advice, minimalism and mindfulness <b>delivered just for you</b>.</small></p>
 
     <div class="form-group">
-      <input type="email" placeholder="What's Your Email?" value="" name="fields[email]" class="email--signup email form-control form-control-sm">
+      <input type="email" placeholder="What's Your Email?" value="" name="fields[email]" class="email--signup email form-control form-control-sm" />
     </div>
-    <p class="mb-0">
-      <small class="pl-2">
-        <input type="radio" name="fields[subscription]" value="all" id="email--subscription-all" checked="checked" /> <label for="email--subscription-all" class="pl-1"><small>All Articles</small></label>
-      </small>
-      <small class="pl-3">
-        <input type="radio" name="fields[subscription]" value="weekly" id="email--subscription-weekly" /> <label for="email--subscription-weekly" class="pl-1"><small>Weekly Magazine</small></label>
-      </small>
-    </p>
-    <p class="px-1 mb-1 gdpr"><small><input type="checkbox" name="fields[eu_consent]" id="drip-eu-consent" value="granted"> <label for="drip-eu-consent">I consent to receive information via email.</label></small></p>
+    <div class="email--toggle mb-0" style="display: none;">
+      <p class="px-1 mb-1">
+        <input type="name" placeholder="What's your first name?" value="" name="fields[name]" class="email--signup name form-control form-control-sm" />
+      </p>
+      <p class="mb-0">
+        <small class="pl-2">
+          <input type="radio" name="fields[subscription]" value="all" id="email--subscription-all" checked="checked" /> <label for="email--subscription-all" class="pl-1"><small>All Articles</small></label>
+        </small>
+        <small class="pl-3">
+          <input type="radio" name="fields[subscription]" value="weekly" id="email--subscription-weekly" /> <label for="email--subscription-weekly" class="pl-1"><small>Weekly Magazine</small></label>
+        </small>
+      </p>
+      <p class="px-1 mb-1 gdpr"><small><input type="checkbox" name="fields[eu_consent]" id="drip-eu-consent" value="granted"> <label for="drip-eu-consent">I consent to receive information via email.</label></small></p>
+    </div>
     <div class="group">
       <button type="submit" class="btn btn-sm btn-success btn-block email-submit"><i class="fa fa-envelope pr-2"></i> Sign me up</button>
     </div>
