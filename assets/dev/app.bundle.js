8658dbfb1715bbbbd386e49524bcf3f41eb151b9
Adam Fortuna <adam@fortuna.name>
2018-06-18 10:35:25 -0700

    Removed 2nd gdpr notice

--- assets/dev/app.bundle.js
+++ assets/dev/app.bundle.js
@@ -6645,24 +6645,24 @@ return Popper;
       $('.email--toggle').slideDown();
     });
 
-    var gdprConsent = Cookies.get("gdpr-consent");
-    var accepts = ["Ok, Thanks"];
-    if(accepts.indexOf(gdprConsent) == -1) {
-      $("body").append("<div class='gdpr-consent-form fixed-top alert alert-success' style='display: none;z-index:100343;'><p class='container text-center mb-0'><small>You probably don't care, but <a href='/privacy-policy'>Minafi uses cookies</a>.</small> <button class='gdpr-consent-button btn-primary btn-sm ml-3'>Ok, Thanks</button></p></div>");
-
-      setTimeout(function() {
-        $(".gdpr-consent-form").slideDown();
-      }, 1000);
-    }
-
-    $("body").on("click", ".gdpr-consent-button", function() {
-      var compliance = $(this).text();
-      Cookies.set("gdpr-consent", compliance);
-      if(accepts.indexOf(compliance) !== -1) {
-        Cookies.set("gdpr-consent-time", +(new Date()));
-        $(".gdpr-consent-form").slideUp();
-      }
-    });
+    // var gdprConsent = Cookies.get("gdpr-consent");
+    // var accepts = ["Ok, Thanks"];
+    // if(accepts.indexOf(gdprConsent) == -1) {
+    //   $("body").append("<div class='gdpr-consent-form fixed-top alert alert-success' style='display: none;z-index:100343;'><p class='container text-center mb-0'><small>You probably don't care, but <a href='/privacy-policy'>Minafi uses cookies</a>.</small> <button class='gdpr-consent-button btn-primary btn-sm ml-3'>Ok, Thanks</button></p></div>");
+    //
+    //   setTimeout(function() {
+    //     $(".gdpr-consent-form").slideDown();
+    //   }, 1000);
+    // }
+    //
+    // $("body").on("click", ".gdpr-consent-button", function() {
+    //   var compliance = $(this).text();
+    //   Cookies.set("gdpr-consent", compliance);
+    //   if(accepts.indexOf(compliance) !== -1) {
+    //     Cookies.set("gdpr-consent-time", +(new Date()));
+    //     $(".gdpr-consent-form").slideUp();
+    //   }
+    // });
 
     if($('.article--toc-list').length > 0) {
       $.each($('.article--main-content h2'), function(i, el) {
