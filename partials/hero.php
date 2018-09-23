ae05dcc7faedd0de162b42c309cbd70c8dc4a22e
Adam Fortuna <adam@fortuna.name>
2018-09-23 17:29:20 -0600

    Fix comment box css

--- partials/hero.php
+++ partials/hero.php
@@ -5,8 +5,9 @@
       <div class='col-xl-6 col-lg-8 col-md-8 col-sm-12'>
         <div class="home--hero-email card p-3 m-2 mb-3">
           <h1 class="h2">Welcome to <?php echo get_bloginfo("name"); ?>!</h1>
-          <p>I'm Adam - a software developer turned investor. I have a lot of <a href="/goals">personal goals</a>, share way too much about my <a href="https://minafi.com/tag/investment-report">personal net worth</a> and write a lot about how to <a href="/category/investing">invest</a> and <a href="/interactive-guide-early-retirement-financial-independence">retire early</a>.
-            <a href="/about"><small>read more...</small></a>
+          <p>
+            Hi, I'm Adam! I help people make <b>informed investment decisions</b> to pursue their best lives through minimalism, mindfulness and financial independence.
+            <a href="/about"><small>Read more...</small></a>
           </p>
           <?php include('email-sidebar.php'); ?>
         </div>
